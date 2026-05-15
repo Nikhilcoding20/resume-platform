cbbe66c786e3e375bce0285a64de650e183cd5db
Nikhil Gangwani <nikhil@Nikhils-MacBook-Pro.local>
2026-05-15 15:27:15 -0400

    add Google Tag Manager and fix education section

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,12 +54,22 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WR7M98XK');`,
+          }}
+        />
         <link rel="icon" href="/favicon.png" type="image/png" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} flex min-h-screen flex-col overflow-x-hidden bg-white text-[#0f172a] antialiased`}
       >
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WR7M98XK" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
         <div className="flex-1">
           {children}
         </div>
